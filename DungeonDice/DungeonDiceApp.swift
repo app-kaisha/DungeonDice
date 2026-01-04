81916ecbd567408e3f6f5d2a8ebe29a5ac41b4a9
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-04 15:05:05 +0000

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,18 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by app-kaihatsusha on 04/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
