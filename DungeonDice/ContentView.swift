172981e1c6f0b3db7d7a4709b3dd3d4c4d16795b
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-04 18:17:54 +0000

    Ch4.6 animate the result text with flips and spins

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var resultMessage = ""
+    @State private var animationTrigger = false
+    @State private var isDoneAnimating = true
     
     enum Dice: Int, CaseIterable, Identifiable {
         
@@ -42,13 +44,23 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.medium)
                 .multilineTextAlignment(.center)
+//                .scaleEffect(isDoneAnimating ? 1.0 : 0.6)
+//                .opacity(isDoneAnimating ? 1.0 : 0.2)
+                .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: (x: 1, y: 0, z: 0))
                 .frame(height: 150)
+                .onChange(of: animationTrigger) {
+                    isDoneAnimating = false
+                    withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
+                        isDoneAnimating = true
+                    }
+                }
             
             Spacer()
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
                 ForEach(Dice.allCases) { die in
                     Button(die.description) {
                         resultMessage = "You rolled a \(die.roll()) on a \(die.rawValue)-sided dice"
+                        animationTrigger.toggle()
                     }
                 }
                 .buttonStyle(.borderedProminent)
