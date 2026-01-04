5030630cc7be30edc13387bfef79ff0866e002f4
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-04 15:42:05 +0000

    Utilise ForEach to reduce code and remove repeating button code

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -37,42 +37,14 @@ struct ContentView: View {
             
             Spacer()
             
-            Group {
-                HStack {
-                    Button("\(Dice.four.rawValue)-side") {
-                        resultMessage = "You rolled a \(Dice.four.roll()) on a \(Dice.four.rawValue)-sided dice"
-                    }
-                    Spacer()
-                    Button("\(Dice.six.rawValue)-side") {
-                        resultMessage = "You rolled a \(Dice.six.roll()) on a \(Dice.six.rawValue)-sided dice"
-                    }
-                    Spacer()
-                    Button("\(Dice.eight.rawValue)-side") {
-                        resultMessage = "You rolled a \(Dice.eight.roll()) on a \(Dice.eight.rawValue)-sided dice"
-                    }
+            ForEach(Dice.allCases, id: \.self) { die in
+                Button("\(die.rawValue)-side") {
+                    resultMessage = "You rolled a \(die.roll()) on a \(die.rawValue)-sided dice"
                 }
-                
-                HStack {
-                    Button("\(Dice.ten.rawValue)-side") {
-                        resultMessage = "You rolled a \(Dice.ten.roll()) on a \(Dice.ten.rawValue)-sided dice"
-                    }
-                    Spacer()
-                    Button("\(Dice.twelve.rawValue)-side") {
-                        resultMessage = "You rolled a \(Dice.twelve.roll()) on a \(Dice.twelve.rawValue)-sided dice"
-                    }
-                    Spacer()
-                    Button("\(Dice.twenty.rawValue)-side") {
-                        resultMessage = "You rolled a \(Dice.twenty.roll()) on a \(Dice.twenty.rawValue)-sided dice"
-                    }
-                }
-                
-                Button("\(Dice.hundred.rawValue)-side") {
-                    resultMessage = "You rolled a \(Dice.hundred.roll()) on a \(Dice.hundred.rawValue)-sided dice"
-                }
-                
             }
             .buttonStyle(.borderedProminent)
             .tint(.red)
+            
         }
         .padding()
     }
