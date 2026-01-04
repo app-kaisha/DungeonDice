03bc84e19dd4f74211596a1c8433b51ef160d2b7
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-04 20:11:04 +0000

    Dice buttons arrangement updated to have central positioned last row

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -14,6 +14,12 @@ struct ContentView: View {
     @State private var animationTrigger = false
     @State private var isDoneAnimating = true
     
+    @State private var buttonsLeftOver = 0
+    
+    let horizontalPadding: CGFloat = 16
+    let spacing: CGFloat = 0
+    let buttonWidth: CGFloat = 102
+    
     enum Dice: Int, CaseIterable, Identifiable {
         
         case four = 4, six = 6, eight = 8, ten = 10, twelve = 12, twenty = 20, hundred = 100
@@ -32,44 +38,80 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
-            Text("Dungeon Dice")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundStyle(.red)
-            
-            Spacer()
-            
-            Text(resultMessage)
-                .font(.largeTitle)
-                .fontWeight(.medium)
-                .multilineTextAlignment(.center)
-//                .scaleEffect(isDoneAnimating ? 1.0 : 0.6)
-//                .opacity(isDoneAnimating ? 1.0 : 0.2)
-                .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: (x: 1, y: 0, z: 0))
-                .frame(height: 150)
-                .onChange(of: animationTrigger) {
-                    isDoneAnimating = false
-                    withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
-                        isDoneAnimating = true
+        GeometryReader { geo in
+            VStack {
+                Text("Dungeon Dice")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.red)
+                
+                Spacer()
+                
+                Text(resultMessage)
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.center)
+                //                .scaleEffect(isDoneAnimating ? 1.0 : 0.6)
+                //                .opacity(isDoneAnimating ? 1.0 : 0.2)
+                    .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: (x: 1, y: 0, z: 0))
+                    .frame(height: 150)
+                    .onChange(of: animationTrigger) {
+                        isDoneAnimating = false
+                        withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
+                            isDoneAnimating = true
+                        }
                     }
-                }
-            
-            Spacer()
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
-                ForEach(Dice.allCases) { die in
-                    Button(die.description) {
-                        resultMessage = "You rolled a \(die.roll()) on a \(die.rawValue)-sided dice"
-                        animationTrigger.toggle()
+                
+                Spacer()
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: buttonWidth), spacing: spacing)]) {
+                    ForEach(Dice.allCases.dropLast(buttonsLeftOver)) { die in
+                        Button(die.description) {
+                            resultMessage = "You rolled a \(die.roll()) on a \(die.rawValue)-sided dice"
+                            animationTrigger.toggle()
+                        }
+                        .frame(width: buttonWidth)
                     }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
                 }
-                .buttonStyle(.borderedProminent)
-                .tint(.red)
+                // to position buttons centrally on last row if needed
+                HStack {
+                    ForEach(Dice.allCases.suffix(buttonsLeftOver)) { die in
+                        Button(die.description) {
+                            resultMessage = "You rolled a \(die.roll()) on a \(die.rawValue)-sided dice"
+                            animationTrigger.toggle()
+                        }
+                        .frame(width: buttonWidth)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
+                }
+                
             }
-            
+            .padding()
+            .onChange(of: geo.size.width) {
+                arrangeGridItems(geo: geo)
+            }
+            .onAppear {
+                arrangeGridItems(geo: geo)
+            }
         }
-        .padding()
+        
     }
+    
+    func arrangeGridItems(geo: GeometryProxy) {
+        var screenWidth = geo.size.width - horizontalPadding * 2 // default padding
+        if Dice.allCases.count > 1 {
+            screenWidth += spacing
+        }
+        // calculate number of buttons per row
+        let numberOfButtonsPerRow = Int(screenWidth) / Int(buttonWidth)
+        buttonsLeftOver = Dice.allCases.count % numberOfButtonsPerRow
+//        print("numberOfButtonsPerRow: \(numberOfButtonsPerRow)")
+//        print("buttonsLeftOver = \(buttonsLeftOver)")
+
+    }
+    
 }
 
 #Preview {
